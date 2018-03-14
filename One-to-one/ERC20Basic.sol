fdd4512632b360d6f53f06645bf0c2c9152aaa31
alexius595 <alexius595@yandex.ru>
2018-03-14 23:14:36 +0300

    проверка

--- One-to-one/ERC20Basic.sol
+++ One-to-one/ERC20Basic.sol
@@ -1,11 +1,3 @@
-
-pragma solidity ^0.4.20;
-
-/**
- * @title ERC20Basic
- * @dev Simpler version of ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/179
- */
 contract ERC20Basic {
   uint256 public totalSupply;
   function balanceOf(address who) public constant returns (uint256);
