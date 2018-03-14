3a5344c023478e2ad3e4c328f75cf432839c78c0
alexius595 <alexius595@yandex.ru>
2018-03-14 23:35:06 +0300

    проверка

--- One-to-one/ERC20Basic.sol
+++ One-to-one/ERC20Basic.sol
@@ -3,4 +3,6 @@ contract ERC20Basic {
   function balanceOf(address who) public constant returns (uint256);
   function transfer(address to, uint256 value) public returns (bool);
   event Transfer(address indexed from, address indexed to, uint256 value);
-} 
+} This reverts commit afd03122f6c11973ffd4d36d6d3dcd64b640d151.
+
+//проверка import
